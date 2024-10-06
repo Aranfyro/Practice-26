5e64adca99c73130f645221c5dfa5d338947298a
Aranfyro <141035777+Aranfyro@users.noreply.github.com>
2024-10-06 18:11:08 +0300

    Add files via upload

---
+++ module_5_4.py
@@ -0,0 +1,29 @@
+# Домашняя работа по уроку "Различие атрибутов класса и экземпляра."
+
+class House:
+    houses_history = []
+
+    def __new__ (cls, *args, **kwargs):
+        name = args[0]
+        cls.houses_history.append(name)
+        return super().__new__(cls)
+
+    def __init__ (self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __del__ (self):
+        print(f'{self.name} снесён,но он останется в истории')
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+# Удаление объектов
+del h2
+del h3
+
+print(House.houses_history)
